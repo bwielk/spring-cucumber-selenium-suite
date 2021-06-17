9242e51375239cb99c0cc86d737e1e84785556f7
Blaise Wielk <bwielk@gmail.com>
2021-06-17 07:29:37 +0100

    simplified the scenario steps - removed the regex

--- src/test/java/com/springcucumberselenium/bdd/cucumber/steps/LoginSteps.java
+++ src/test/java/com/springcucumberselenium/bdd/cucumber/steps/LoginSteps.java
@@ -19,52 +19,52 @@ public class LoginSteps {
     @Lazy
     private UserPortalPage userPortalPage;
 
-    @Given("the login page is open$")
+    @Given("the login page is open")
     public void  theLoginPageIsOpen(){
         homePage.openLoginPage();
     }
 
-    @Given("a user has entered a wrong username \\\"([^\\\"]*)\\\"$")
+    @Given("a user has entered a wrong username {}")
     public void userEnteredAWrongUsername(String wrongUsername){
         homePage.fillUsernameInput(wrongUsername);
     }
 
-    @And("the user has entered an invalid password \\\"([^\\\"]*)\\\"$")
+    @And("the user has entered an invalid password {}")
     public void userEnteredAWrongPassword(String wrongPassword){
         homePage.fillPasswordInput(wrongPassword);
     }
 
-    @When("the user hits the submit button$")
+    @When("the user hits the submit button")
     public void submitButtonHit(){
         homePage.clickLogInButton();
     }
 
-    @Then("the user sees an error$")
+    @Then("the user sees an error")
     public void userSeesError(){
         homePage.invalidLoginDataErrorDisplayed();
     }
 
-    @And("the user is not logged in$")
+    @And("the user is not logged in")
     public void userNotLoggedIn(){
         userPortalPage.checkUserIsNotLoggedIn();
     }
 
-    @Given("a user has entered a valid username$")
+    @Given("a user has entered a valid username")
     public void userHasEnteredTheCorrectUsername(){
         homePage.fillUsernameInput(System.getProperty("username"));
     }
 
-    @And("the user has entered a valid password$")
+    @And("the user has entered a valid password")
     public void userHasEnteredTheCorrectPassword(){
         homePage.fillPasswordInput(System.getProperty("password"));
     }
 
-    @Then("the user is logged in$")
+    @Then("the user is logged in")
     public void userHasLoggedIn(){
         userPortalPage.checkUserIsLoggedIn();
     }
 
-    @And("the user sees the user panel$")
+    @And("the user sees the user panel")
     public void userSeesTheUserPanel(){
         userPortalPage.checkUserPanelIsDisplayed();
     }
--- src/test/resources/login/Login.feature
+++ src/test/resources/login/Login.feature
@@ -4,8 +4,8 @@
       Given the login page is open
 
     Scenario: Failed login with both invalid credentials
-      Given a user has entered a wrong username "abc"
-      And the user has entered an invalid password "def"
+      Given a user has entered a wrong username abc
+      And the user has entered an invalid password def
       When the user hits the submit button
       Then the user sees an error
       And the user is not logged in
